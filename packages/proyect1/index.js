c0f1aabe1a3c99ad64c882fe71ac59a4372c2a3c
webdev4Centauro <72547367+webdeveloper4Centauro@users.noreply.github.com>
2020-11-02 10:12:50 +0100

    dependencias montadas

---
+++ packages/proyect1/index.js
@@ -0,0 +1,5 @@
+const proyect2 = require('proyect2');
+const proyect3 = require('proyect3');
+const proyect4 = require('proyect4');
+
+console.log(`proyect1: ${proyect2}, ${proyect3}, ${proyect4}`);
